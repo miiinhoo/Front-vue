71a3c96ad67419972a2fb95819c3369d4c7dcf88
miiinhoo <154577329+miiinhoo@users.noreply.github.com>
2025-10-20 18:03:16 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import { ref } from 'vue';
+  import Hello from './components/pages/Hello.vue';
+
+  //-----------------
+  const number = ref('');
+
+  function updateNumber(num){
+    number.value += num;
+  }
+  function resetBtn(){
+    number.value = '';
+  }
+</script>
+
+<template>
+  <Hello
+    v-model:num="number"
+    v-model:update="updateNumber"
+    v-model:reset="resetBtn"
+  />
+
+  <p>
+    <br>
+    테스트
+    <br>
+    결과 : {{ first }} {{ operator }} {{ second }} = {{ operEqaul() }}
+  </p>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/pages/Hello.vue
@@ -0,0 +1,26 @@
+<template>
+  
+</template>
+<script setup>
+
+
+
+  // ------------------------
+
+  const prop = defineProps({
+    num: Number,
+    update: Number,
+    reset: Number,
+  });
+  const emit = 
+  defineEmits([
+    'update:num',
+    'update:update',
+    'update:reset'
+  ]);
+
+  function firstNumber(data){
+    emit('update:one',Number(data));
+  }
+  
+</script>
